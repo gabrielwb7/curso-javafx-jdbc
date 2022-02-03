61efb189296aca30760082615bf407af46a276f6
Gabriel Rocha <fernandeswb7@hotmail.com>
2022-02-02 21:39:41 -0300

    Create Project

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module javafx {
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+	
+	opens gui to javafx.graphics, javafx.fxml;
+}
